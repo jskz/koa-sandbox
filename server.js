141124f2c14e2be9ad4e8e4575cf75dc6ce5135e
James Skarzinskas <james@jskarzin.org>
2015-12-23 13:01:52 -0500

    koa-static, koa-basic-auth

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 import koa from 'koa'
+import koaBasicAuth from 'koa-basic-auth'
+import koaStatic from 'koa-static'
 
 let app = koa()
 
+const HTML = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Koa Sandbox</title>
+</head>
+
+<body>
+</body>
+</html>`
+
+app.use(koaBasicAuth)
+app.use(koaStatic(__dirname + '/static'))
+
 app.use(function *(next) {
-    console.log('Test middleware.')
-    next()
+    console.log('Test middleware - pre-yield.')
+    yield next;
+    console.log('Test middleware - post-yield.')
 })
 
 app.use(function *() {
+    console.log('Setting responsive body to `Hello Koa!`')
+    console.log(this)
+
     this.body = `Hello Koa!`
 })
 
